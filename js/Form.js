678c99f0a26f673d483dac026d88b75bad1a5bb6
unknown <saralaswarna.s@gmail.com>
2021-07-21 14:19:27 +0530

    reset and marking player

--- js/Form.js
+++ js/Form.js
@@ -1,10 +1,11 @@
 class Form {
 
   constructor() {
-    this.input = createInput("Name");
+    this.input = createInput("").attribute("placeholder","Name");
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('reset');
   }
   hide(){
     this.greeting.hide();
@@ -19,7 +20,14 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
+
+    this.reset.position(displayWidth-100,20);
+    this.reset.mousePressed(()=>{
+
+      player.updateCount(0);
+      game.update(0);
+    })
+
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
@@ -33,4 +41,5 @@ class Form {
     });
 
   }
+
 }
